ea791bec4092bcc2eede42159cb7213fec5f85e7
hoppyhopps <hoppyhopps@proton.me>
2024-10-07 11:49:35 -0400

    cleanup proofs

--- src/main.rs
+++ src/main.rs
@@ -29,18 +29,17 @@ fn crit_math(damage: f32, c_damage: f32, c_rate: f32) {
     // thus we can find our actual damage increase with the expression d = T/N
 
     println!("\nwith our variables b for base damage c for critical damage and r for critical rate critical damage can be expressed as b * [1 + r * c]");
-    println!("average damage increase for crits over a large number of hits is then expressed as");
-    println!("T = [1 - r] * N * b + r * N * b * [1 + c]");
+    println!("average damage increase for crits over a large number of hits is then expressed as T = [1 - r] * N * b + r * N * b * [1 + c]");
     println!("thus we can find our actual damage increase with the expression d = T/N, with N being 10000");
     let b: f32 = damage;
     let c: f32 = c_damage;
     let r: f32 = c_rate;
     let n: f32 = 10000.0;
-    println!("(1 - {}) * {} * {} + {} * {} * {} * (1 + {})", r, n, b, r, n, b, c);
+    println!("t = (1 - {}) * {} * {} + {} * {} * {} * (1 + {})", r, n, b, r, n, b, c);
     let t: f32 = (1.0 - r) * n * b + r * n * b * (1.0 + c);
 
     let mut average_damage: f32 = t/n;
-    println!("Average damage of a {} damage attack with a critical multipler of {} and a crit rate of {} is {}", b, c, r, average_damage);
+    println!("Average damage, which is T/N, of a {} damage attack with a critical multipler of {} and a crit rate of {} is {}", b, c, r, average_damage);
 
     // this can be further simplified to the equation b * [1 + r * c], to prove this we do the following
     println!("this can be further simplified to the equation b * [1 + r * c], to prove this we do the following");
